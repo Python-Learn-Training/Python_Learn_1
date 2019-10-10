0c3c87efc9041ba55bd1e45c0d49c32ee755c4a4
Fish Chen <lovefish0416@gmail.com>
2019-10-10 13:51:38 +0800

    data: 程式基本單位

--- start.py
+++ start.py
@@ -1,1 +1,18 @@
+# 第一支程式 
+# 190804 副檔名用.py
+# 執行程式: python 檔案名稱.py
+# 清空終端機的資料：cls
 print ("Hello Python")
+
+#data: 程式基本單位
+#有順序、可變動列表 List
+[3,4,5]
+["Hello","World"]
+#有順序、不可變動列表 Tuple
+(3,4,5)
+("Hello","World")
+#集合 Set
+{3,4,5}
+{"Hello","World"}
+#字典 Dictionary {"鍵1":"值1","鍵2":"值2"}
+{"banana":"香蕉","data":"資料"}
