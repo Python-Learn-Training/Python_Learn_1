61c4dc4ca743c09469cea65a167305947b8c2045
Fish Chen <lovefish0416@gmail.com>
2019-11-02 15:53:45 +0800

    if  elif  else

--- condition.py
+++ condition.py
@@ -1,7 +1,9 @@
 #判斷式
 x=input("Please input number : ") #取得字串
 X=int(x) #將字串轉成數字
-if X>100:
-    print("X>100")
+if X>200:
+    print("X<200")
+elif X>100:
+    print("X>100 X<200")
 else:
     print("X<= 100")
