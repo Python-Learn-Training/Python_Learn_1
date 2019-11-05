266e79a53fe71ae58a7472d5871e6e534432d3a6
Fish Chen <lovefish0416@gmail.com>
2019-11-05 08:51:33 +0800

    While&for迴圈

---
+++ loop-basic.py
@@ -0,0 +1,20 @@
+#while迴圈
+#1+2+3...+10
+#n=1
+#sum=0 #紀錄累加結果
+#while n<=10:
+#    sum=sum+n
+#    n+=1
+#print(sum)
+
+#for迴圈
+for x in [3,5,1] :
+    print("x=" , x)
+
+for y in range(5):
+    print("y=",y)
+
+sum=0
+for z in range(1,11):
+    sum=sum+z
+print("sum z =",sum)
